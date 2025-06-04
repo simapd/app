ec7d8cd5587cc151feaac0f50967bf057480b7e0
guimaggiorini <guimaggiorini@gmail.com>
2025-06-04 19:17:01 -0300

    feat(home): add mocked header

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,5 +1,5 @@
 import { Text } from '@/components/ui/text'
-import { ScrollView } from 'react-native'
+import { ScrollView, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function Home() {
@@ -15,7 +15,24 @@ export default function Home() {
         }}
         showsVerticalScrollIndicator={false}
       >
-        <Text>Hello World!</Text>
+        <View className="flex flex-col items-center justify-center gap-3 py-10">
+          <Text className="text-xl">Morro do Macaco</Text>
+          <View className="flex flex-row items-baseline justify-center gap-2 relative">
+            <Text className="text-7xl font-semibold">78</Text>
+            <Text className="text-xl absolute left-24 top-7">/100</Text>
+          </View>
+          <View className="flex flex-col items-center justify-center gap-1">
+            <Text className="text-xl leading-none">Evite áreas de encosta</Text>
+            <View className="flex flex-row items-center justify-center gap-6">
+              <Text className="text-xl">
+                U: <Text className="text-xl font-semibold">72%</Text>
+              </Text>
+              <Text className="text-xl">
+                C: <Text className="text-xl font-semibold">82mm</Text>
+              </Text>
+            </View>
+          </View>
+        </View>
       </ScrollView>
     </SafeAreaView>
   )
